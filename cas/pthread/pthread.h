c385ea26c0be7e935d6ee1d0a77e9ca32fe9b99d
Christian Schreiner <cas@cs.utah.edu>
2014-04-04 04:43:15 -0600

    added template

---
+++ cas/pthread/pthread.h
@@ -0,0 +1,49 @@
+/*** 
+   * Program Name: SMACK pthreads
+   *
+   * File Name: pthread.h
+   *
+   * File Description: 
+   *
+   * SMACK pthreads was written by Christian A. Schreiner at University of
+   * Utah.  Copyright (C) 2014-2014 by University of Utah.  All rights
+   * reserved.  You may use, examine, or modify this file only in accordance
+   * with the GNU Public License, or, alternately, by special written
+   * arrangement with the author.  This file comes with no warranties.  If you
+   * use it and something breaks, you are solely responsible for cleaning up
+   * afterwards.
+   *
+   */
+
+/*** Configuration Management Information:
+   * 
+   * $Source: /home/cas/templates/auto_uut/RCS/cas_tmpl.h,v $
+   * $Author: cas $
+   * $Date: 2014/04/04 10:34:34 $
+   * $Revision: 1.3 $
+   * 
+   */
+
+#if !defined(__PTHREAD_H__)
+#define __PTHREAD_H__
+
+/*** **************************************************************************
+   *   includes
+   * **************************************************************************
+   */
+
+/*** **************************************************************************
+   *   declarations
+   * **************************************************************************
+   */
+
+/*** ==========================================================================
+   *   function prototypes
+   */
+
+/*** **************************************************************************
+   *   end of file
+   * **************************************************************************
+   */
+#endif /* !defined(__PTHREAD_H__) */
+
