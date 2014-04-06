562d84d9914f7d737f33ad5b0d8acddb0a104c37
Christian Schreiner <cas@cs.utah.edu>
2014-04-05 22:59:09 -0600

    try capitalizing ASYNC, this is how Zvonimir did it in his example

--- cas/pthread/test_parts/spthread_parts.h
+++ cas/pthread/test_parts/spthread_parts.h
@@ -151,8 +151,8 @@ int spthread_create( spthread_t* thread_ptr, const spthread_attr_t* attr_ptr,
    }
    *thread_ptr= &_spthread_ctl_array[ii];
    /* TODO: refine the thread start code here */ 
-   //;;__SMACK_code( "call {:async} spthread_ftn_wrapper( " 
-   //;;	 "thread_ptr, start_routine_ptr, arg_ptr );" ); 
+   __SMACK_code( "call {:ASYNC} spthread_ftn_wrapper( " 
+   	 "thread_ptr, start_routine_ptr, arg_ptr );" ); 
    return 0;
 }}
 
