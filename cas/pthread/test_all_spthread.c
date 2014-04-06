3bb8d35720bdb16bb66486b3436933d18baa4e01
Christian Schreiner <cas@cs.utah.edu>
2014-04-05 22:34:56 -0600

    began changing to use spthread.h, instead of its own pthread routines

--- cas/pthread/test_all_spthread.c
+++ cas/pthread/test_all_spthread.c
@@ -7,6 +7,8 @@
 #include "/uusoc/scratch/euler/cas/tuut/x86_64/smack-project/smack/install/include/smack/smack.h"
 #include "spthread.h"
 
+spthread_attr_t attrs;
+
 int x = 1;
 
 void *my_procedure(void *my_argument) {
@@ -20,9 +22,9 @@ void *another(void *my_argument) {
 }
 
 int main() {
-   pthread_t my_procedures_thread_ctl, another_thread_ctl;
-   spthread_create( &my_procedures_thread_ctl, my_procedure, 0);
-   spthread_create( &another_thread_ctl, another, 0);
+   spthread_t my_procedures_thread_ctl, another_thread_ctl;
+   spthread_create( &my_procedures_thread_ctl, &attrs, my_procedure, 0);
+   spthread_create( &another_thread_ctl, &attrs, another, 0);
    return 0;
 }
 
