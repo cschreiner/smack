c6f646b3932adef54946e445fde0c6ef86b921e9
Christian Schreiner <cas@cs.utah.edu>
2014-04-10 14:27:52 -0600

    adapted to use pthreads (I hope)

--- cas/pthread/test_storm/account_pthread.c
+++ cas/pthread/test_storm/account_pthread.c
@@ -1,5 +1,6 @@
-#include "locks.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include "../spthread.h"
 
 #define TRUE 1
 #define FALSE 0
@@ -10,68 +11,81 @@
 // Account structure
 typedef struct {
   int balance;
-  KSPIN_LOCK lock;
-} ACCOUNT, *PACCOUNT;
+  spthread_mutex_t lock;
+} account_t, *account_ptr_t;
 
 
-PACCOUNT create(int b) {
-  PACCOUNT acc = (PACCOUNT) malloc(sizeof(ACCOUNT));
+account_ptr_t create(int b) {
+  account_ptr_t acc = (account_ptr_t) malloc(sizeof(account_t));
   acc->balance = b;
-  InitializeSpinLock(&acc->lock);
+  spthread_mutex_init(&(acc->lock), SPTHREAD_MUTEX_INITIALIZER );
   return acc;
 }
 
-int read(PACCOUNT acc) {
+int read(account_ptr_t acc) {
   return acc->balance;
 }
 
-void deposit(PACCOUNT acc, int n) {
-  AcquireSpinLock(&acc->lock);
+void deposit(account_ptr_t acc, int n) {
+  spthread_mutex_lock(&acc->lock);
   acc->balance = acc->balance + n;
-  ReleaseSpinLock(&acc->lock);
+  spthread_mutex_unlock(&acc->lock);
 }
 
-void withdraw(PACCOUNT acc, int n) {
+void withdraw(account_ptr_t acc, int n) {
   int r;
-  AcquireSpinLock(&acc->lock);
-  r = read(acc);
-//  AcquireSpinLock(&acc->lock);
+  spthread_mutex_lock(&acc->lock);
+  r = read(acc); // why are we storing balance in a local var?
   acc->balance = r - n;
-  ReleaseSpinLock(&acc->lock);
+  spthread_mutex_unlock(&acc->lock);
 }
 
 
+// =========================================================================
 // Harness
 
+typedef struct {
+   account_ptr_t acc;
+   int amt;
+} thread_arg_t;
+
 // Thread 1
-void deposit_thread(PACCOUNT acc, int n) {
-  deposit(acc, n);
+void deposit_thread( thread_arg_t* aa ) {
+  deposit( aa->acc, aa->amt );
 }
 
 // Thread 2
-void withdraw_thread(PACCOUNT acc, int n) {
-  withdraw(acc, n);
+void withdraw_thread( thread_arg_t* aa ) {
+  withdraw( aa->acc, aa->amt );
 }
 
 
-void storm_main() {
-  PACCOUNT acc;
+// =========================================================================
+void main() {
+  account_ptr_t acc;
   int x, y, z;
+  spthread_t deposit_thread_ctl, withdraw_thread_ctl;
+  thread_arg_t deposit_args, withdraw_args;
+  int* retval;
 
   // Initialization
   x = nondet();
   y = nondet();
   z = nondet();
   acc = create(x);
+  deposit_args= { acc, y );
+  withdraw_args= { acc, z );
+
 
   // Threads
-  __async_call deposit_thread(acc, y); // deposit
+  spthread_create( &deposit_thread_ctl, NULL, deposit_thread, &deposit_args  );
+  spthread_create( &withdraw_thread_ctl, NULL, withdraw_thread, &withdraw_args );
+  spthread_join( deposit_thread_ctl, &retval );
+  spthread_join( withdraw_thread_ctl, &retval );
 
-  __async_call withdraw_thread(acc, z); // withdraw
-
-  STORM_END;
-  STORM_ASSERT(read(acc) == x + y - z);
+  __SMACK_assert(read(acc) == x + y - z);
 }
 
-void main() {}
 
+// =========================================================================
+// end of file
