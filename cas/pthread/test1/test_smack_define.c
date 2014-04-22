f21f2808d23e46a02b902aa336deadf3e4d8f47c
Christian Schreiner <cas@cs.utah.edu>
2014-04-21 20:27:47 -0600

    added main() call

--- cas/pthread/test1/test_smack_define.c
+++ cas/pthread/test1/test_smack_define.c
@@ -1,8 +1,14 @@
 /* test to see if SMACK is being defined as I understand it should.
 */
+#include <stdio.h>
 #include "/uusoc/scratch/euler/cas/tuut/x86_64/smack-project/smack/install/include/smack/smack.h"
 
 #ifdef SMACK
    #error SMACK is defined
 #endif
+
+int main( int argc, char* argv[] )
+{{
+   printf( "Hello, world. \n" );
+}}
 
