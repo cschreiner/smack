4447928b21a3d7dc2244c18b34ef4196e601c911
Christian Schreiner <cas@cs.utah.edu>
2014-04-21 20:37:21 -0600

    this produces the error messages expected with a gcc compiler, with SMACK defined and with it undefined

--- cas/pthread/test1/test_smack_define.c
+++ cas/pthread/test1/test_smack_define.c
@@ -1,14 +1,20 @@
 /* test to see if SMACK is being defined as I understand it should.
 */
-#include <stdio.h>
 #include "/uusoc/scratch/euler/cas/tuut/x86_64/smack-project/smack/install/include/smack/smack.h"
 
+int printf_stub( char* st );
+
+
 #ifdef SMACK
-   #error SMACK is defined
+   #error "SMACK is defined"
+#else
+   #error "SMACK is not defined"
 #endif
+
+#error "silly message"
 
 int main( int argc, char* argv[] )
 {{
-   printf( "Hello, world. \n" );
+   printf_stub( "Hello, world. \n" );
 }}
 
