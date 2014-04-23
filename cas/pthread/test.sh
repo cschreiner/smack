0ac5cc60e89d2ab0772bdaca61699bf2cb202226
Christian Schreiner <cas@cs.utah.edu>
2014-04-22 19:17:02 -0600

    added ability to check if the output is correct or not

--- cas/pthread/test.sh
+++ cas/pthread/test.sh
@@ -1,14 +1,48 @@
 #!/bin/sh
 # run a standard test suite on the spthread library.
 
-./smack_for_pthreads test1/test_all_spthread.c
-./smack_for_pthreads test1/test_z.c
-./smack_for_pthreads test1/test_z_w_error.c
+function test_one_program
+{
+   arg_program="$1"
+   arg_expected_result="$2"
 
-./smack_for_pthreads test_storm/account_pthread.c
-./smack_for_pthreads test_storm/account_pthread_w_error2.c
-./smack_for_pthreads test_storm/account_pthread_w_error.c
+   test_one_program "$1" | grep -v -e "^Boogie verification time:" \
+         -e "^Time spent reading-writing programs: .* s" \
+         -e "^Time spent checking a program .* s" \
+         -e "^Time spent checking a path .* s" \
+         -e "^Number of procedures inlined: " \
+         -e "^Number of variables tracked: " \
+         -e "^Total Time: .* s" \
+         -e "^ *$" | \
+      grep -q ^
+   status=$?
 
+   if [ "$arg_expected_result" = "ok" ]; then
+      if [ -a $status -eq 1 ]; then
+	 echo "  " \"$arg_program\" corrrectly found no bugs. 
+      else 
+	 echo "  " \"$arg_program\" incorrrectly found bugs. 
+      fi
+   elif [ "$arg_expected_result" = "bug" ]; then
+      if [ -a $status -eq 0 ]; then
+	 echo "  " \"$arg_program\" corrrectly found bugs. 
+      else 
+	 echo "  " \"$arg_program\" incorrrectly found no bugs. 
+      fi
+   fi
+
+   return 0
+}
+
+test_one_program test1/test_all_spthread.c ok
+test_one_program test1/test_z.c ok
+test_one_program test1/test_z_w_error.c bug
+
+test_one_program test_storm/account_pthread.c ok
+test_one_program test_storm/account_pthread_w_error2.c bug
+test_one_program test_storm/account_pthread_w_error.c bug
+
+# end of script
 
 
 
