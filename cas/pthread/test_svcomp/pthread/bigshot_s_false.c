aa7ef41b75bfa847f44d8eb2e336f5430a3e62f3
Christian Schreiner <cas@cs.utah.edu>
2014-04-24 20:53:46 -0600

    adding files from the svcomp website, with customizations for smack

--- cas/pthread/test_svcomp/pthread/bigshot_s_false.c
+++ cas/pthread/test_svcomp/pthread/bigshot_s_false.c
@@ -1,3 +1,6 @@
+/* from
+ * https://svn.sosy-lab.org/software/sv-benchmarks/tags/svcomp14/pthread/bigshot_s_false.c
+*/
 #include <stdlib.h>
 #include "/uusoc/scratch/euler/cas/tuut/x86_64/smack-project/smack/install/include/smack/smack.h"
 #include "/uusoc/scratch/euler/cas/edu/uut/smack/cas/pthread/spthread.h"
@@ -9,13 +12,11 @@ char *v;
 void *thread1(void * arg)
 {
   v = malloc(sizeof(char) * 8);
-  return NULL;
 }
 
 void *thread2(void *arg)
 {
   if (v) strcpy(v, "Bigshot");
-  return NULL;
 }
 
 
@@ -24,11 +25,12 @@ int main()
   spthread_t t1, t2;
 
   spthread_create(&t1, 0, thread1, 0);
-  spthread_create(&t2, 0, thread2, 0);
   spthread_join(t1, 0);
+
+  spthread_create(&t2, 0, thread2, 0);
   spthread_join(t2, 0);
 
-  __SMACK_assert(!v || v[0] == 'B');
+  __SMACK_assert(v[0] == 'B');  // <---- wrong, malloc() can fail and therefore no strcpy!
 
   return 0;
 }
--- cas/pthread/test_svcomp/pthread/bigshot_s_true.c
+++ cas/pthread/test_svcomp/pthread/bigshot_s_true.c
@@ -1,3 +1,7 @@
+/* from:
+https://svn.sosy-lab.org/software/sv-benchmarks/tags/svcomp14/pthread/bigshot_s_true.c
+*/
+
 #include <stdlib.h>
 #include "/uusoc/scratch/euler/cas/tuut/x86_64/smack-project/smack/install/include/smack/smack.h"
 #include "/uusoc/scratch/euler/cas/edu/uut/smack/cas/pthread/spthread.h"
@@ -9,24 +13,23 @@ char *v;
 void *thread1(void * arg)
 {
   v = malloc(sizeof(char) * 8);
-  return NULL;
 }
 
 void *thread2(void *arg)
 {
   if (v) strcpy(v, "Bigshot");
-  return NULL;
 }
 
 
 int main()
 {
-  spthread_t t1, t2;
+  pthread_t t1, t2;
 
-  spthread_create(&t1, 0, thread1, 0);
-  spthread_create(&t2, 0, thread2, 0);
-  spthread_join(t1, 0);
-  spthread_join(t2, 0);
+  pthread_create(&t1, 0, thread1, 0);
+  pthread_join(t1, 0);
+
+  pthread_create(&t2, 0, thread2, 0);
+  pthread_join(t2, 0);
 
   __SMACK_assert(!v || v[0] == 'B');
 
