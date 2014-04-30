c3c2ab62d353ba49cc3487b69e13b7e1d6fc2068
Christian Schreiner <cas@cs.utah.edu>
2014-04-29 20:21:55 -0600

    short program to figure out why gcc and clang seem to include its internal pthread.h instead of the pthread.h I'm specifying

---
+++ cas/pthread/try/pthread_vs_spthread.c
@@ -0,0 +1,14 @@
+
+//#include <pthread.h>
+#include "/uusoc/scratch/euler/cas/edu/uut/smack/cas/pthread/include/smack/pthread.h"
+
+typedef struct {
+   int lock;
+} pthread_mutex_t;
+
+int main( int argc, char* argv[] ) 
+{{
+   int ii= 5;
+}}
+
+
