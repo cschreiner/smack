f687a8392aef8f1316e065396e90cd1ecb3b654c
Christian Schreiner <cas@cs.utah.edu>
2014-04-30 00:27:56 -0600

    removed debugging code

--- cas/pthread/try/pthread_vs_spthread.c
+++ cas/pthread/try/pthread_vs_spthread.c
@@ -2,9 +2,11 @@
 //#include <pthread.h>
 #include "/uusoc/scratch/euler/cas/edu/uut/smack/cas/pthread/include/smack/pthread.h"
 
-typedef struct {
-   int lock;
-} pthread_mutex_t;
+#if 0 //debugging
+   typedef struct {
+      int lock;
+   } pthread_mutex_t;
+#endif
 
 int main( int argc, char* argv[] ) 
 {{
