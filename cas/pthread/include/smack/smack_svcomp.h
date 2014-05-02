28a19cf307ae2caaf4caf0ac2753edfe198c66dc
Christian Schreiner <cas@cs.utah.edu>
2014-05-02 13:58:01 -0600

    header file to define macros and similar stuff needed by sv comp

---
+++ cas/pthread/include/smack/smack_svcomp.h
@@ -0,0 +1,69 @@
+/*** 
+   * Program Name: SMACK pthreads
+   *
+   * File Name: smack_svcomp.h
+   *
+   * File Description: 
+   *
+   * SMACK pthreads was written by Christian A. Schreiner at University of
+   * Utah.  Copyright (C) 2014-2014 by University of Utah.  All rights
+   * reserved.  You may use, examine, or modify this file only in accordance
+   * with the GNU Public License, or, alternately, by special written
+   * arrangement with the author.  This file comes with no warranties.  If you
+   * use it and something breaks, you are solely responsible for cleaning up
+   * afterwards.
+   *
+   */
+
+/*** Configuration Management Information:
+   * 
+   * $Source: /home/cas/templates/auto_uut/RCS/cas_tmpl.h,v $
+   * $Author: cas $
+   * $Date: 2014/05/02 19:51:11 $
+   * $Revision: 1.4 $
+   * 
+   */
+
+#if !defined(__SMACK_SVCOMP_H__)
+#define __SMACK_SVCOMP_H__
+
+/*** **************************************************************************
+   *   includes
+   * **************************************************************************
+   */
+
+/*** **************************************************************************
+   *   declarations
+   * **************************************************************************
+   */
+
+/*** ==========================================================================
+   *   function prototypes
+   */
+
+// template is 17 lines long
+/*** --------------------------------------------------------------------------
+   * function name()
+   * --------------------------------------------------------------------------
+   * Description: 
+   *
+   * Method: 
+   *
+   * Reentrancy: 
+   *
+   * Inputs: 
+   *    
+   * Outputs: 
+   *
+   * Return Value: 
+   *
+   */
+//void name()
+
+
+/*** **************************************************************************
+   *   end of file
+   * **************************************************************************
+   */
+#endif /* !defined(__SMACK_SVCOMP_H__) */
+
