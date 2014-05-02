009db7ecfa4e57181a5a8caf00eb7f4c3ce8e53c
Christian Schreiner <cas@cs.utah.edu>
2014-05-01 20:14:15 -0600

    added __SMACK_INIT() and __SMACK_atomic_begin/end(), and preamble comments

--- cas/pthread/include/smack/smack.h
+++ cas/pthread/include/smack/smack.h
@@ -23,11 +23,94 @@ void __SMACK_assume(bool v) {
 //   __SMACK_code("call boogie_si_record_int(@);", i);
 // }
 
+/*** --------------------------------------------------------------------------
+   * function __SMACK_nondet()
+   * --------------------------------------------------------------------------
+   * Description: returns a non-deterministic value. This is sometimes 
+   *	useful to help verify that a property holds for any legal value. 
+   *
+   * Method:
+   *
+   * Reentrancy:
+   *
+   * Inputs: none
+   * 
+   * Outputs: none
+   *
+   * Return Value: a non-deterministic integer value.  If a subset of the
+   *	integers is desired, follow the call to this function with an
+   *	__SMACK_assume() statement defining the restriction.  For example:
+   *
+   *	xx= __SMACK_nondet();
+   *	__SMACK_assume( 0 <= xx && xx < 100 );
+   */
 int __SMACK_nondet() {
   static int XXX;
   int x = XXX;
   __SMACK_code("havoc @;", x);
   return x;
 }
+
+/*** --------------------------------------------------------------------------
+   * macro __SMACK_INIT()
+   * --------------------------------------------------------------------------
+   * Description: Must be called at the top of a function making certain 
+   *	Smack API functions.  This macro inserts __SMACK_top_decl() calls 
+   *	needed by those functions.
+   *
+   * Method:
+   *
+   * Reentrancy:
+   *
+   * Inputs:
+   * 
+   * Outputs:
+   *
+   * Return Value:
+   *
+   */
+#define __SMACK_INIT() \
+   __SMACK_top_decl( "procedure corral_atomic_begin();" );  \
+   __SMACK_top_decl( "procedure corral_atomic_end();" ); 
+
+
+/*** --------------------------------------------------------------------------
+   * macro __SMACK_atomic_begin()
+   * macro __SMACK_atomic_end()
+   * --------------------------------------------------------------------------
+   * Description: begins (ends) an block of atomic code i.e. code that may
+   *	not be interrupted by task swaps.
+   *
+   * Method: 
+   *
+   * Reentrancy: yes, these are safe to use in a reentrant environment.
+   *
+   * Inputs: none
+   * 
+   * Outputs: none
+   *
+   * Return Value: void
+   *
+   */
+#define __SMACK_atomic_begin() __SMACK_code( "call corral_atomic_begin();" )
+#define __SMACK_atomic_end() __SMACK_code( "call corral_atomic_end();" )
+
+/* templatle is 17 lines long */
+/*** --------------------------------------------------------------------------
+   * function name()
+   * --------------------------------------------------------------------------
+   * Description: 
+   *
+   * Method: 
+   *
+   * Reentrancy: 
+   *
+   * Inputs: 
+   * 
+   * Outputs: 
+   *
+   * Return Value: 
+   *
+   */
 
 #endif /*SMACK_H_*/
